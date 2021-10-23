b32f2e3dd717a15c1fcbcfc4a51062597742ce62
rfqalfauzaan <alfauzaan.rifqi@gmail.com>
2021-10-24 03:57:20 +0700

    Update post.blade.php
    ada yg belum ke save

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -4,13 +4,15 @@
 
 <article>
 <h1 class = "mb-5">{{ $post->title }}</h1>
-<p>By. <a href="/authors/{{ $post->author->username }}" class = "text-decoration-none">{{ $post->author->username }}</a> in <a href="/categories/{{ $post->category->slug }}" class = "text-decoration-none"> {{ $post->category->name }}</a></p>
+<p>by. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a>
+<a href="/categories/{{ $post->category->slug }}"class="text-decoration-none"
+> {{ $post->category->name }}</a></p>
 
 <h5>{{ $post->author }}</h5>
 
 {!! $post->body !!}
 </article>
 
-<a href="/blog" class="d-block mt-3">Back to Posts</a>
+<a href="/blog" class='d-block mt-3'>Back to Posts</a>
 @endsection
 
