2c8e542e0bd49a8dbd01ba8cb3d8d11f9acdac1f
rfqalfauzaan <alfauzaan.rifqi@gmail.com>
2021-11-21 19:28:30 +0700

    18. Create Post Form

--- coba-laravel/resources/views/dashboard/layouts/main.blade.php
+++ coba-laravel/resources/views/dashboard/layouts/main.blade.php
@@ -6,7 +6,6 @@
     <meta name="author" content="Amisha Al azis">
     <title>WPU Blog | Dashboard</title>
 
-    
 
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -14,6 +13,16 @@
 
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+    <!-- Trix Editor -->
+    <link rel="stylesheet" type="text/css" href="/css/trix.css">
+  <script type="text/javascript" src="/js/trix.js"></script>
+
+  <style>
+    trix-toolbar [data-trix-button-group="file-tools"] {
+      display:none;
+    }
+  </style>
   </head>
   <body>
 
